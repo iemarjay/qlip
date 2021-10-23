9e625238cfa2e712c7032055967c92e36fb3bbc1
joseph emmanuel kayode (iemarjay) <emarjay921@gmail.com>
2021-10-23 17:57:26 +0100

    fix pm2:reload task

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -20,7 +20,8 @@ npm run build
 @endtask
 
 @task('pm2:reload')
-export PATH=$PATH:/opt/node/lib/node_modules/pm2/bin
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd {{ $path }}
 pm2 reload {{ $pm2Id }}
 @endtask
